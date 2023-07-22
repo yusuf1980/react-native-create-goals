05edf9c5a09e11911f5bf9de20ed37c4b8466a2b
yusuf1980 <yusufachmad1980@gmail.com>
2023-07-22 11:56:04 +0800

    Making items deletable & using IDs

--- App.js
+++ App.js
@@ -14,8 +14,10 @@ export default function App() {
     ]);
   }
 
-  function deleteGoalHandler() {
-    console.log("DELETE");
+  function deleteGoalHandler(id) {
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id)
+    })
   }
 
   return (
@@ -28,6 +30,7 @@ export default function App() {
             return (
               <GoalItem
                 text={itemData.item.text}
+                id={itemData.item.id}
                 onDeleteItem={deleteGoalHandler}
               />
             );
