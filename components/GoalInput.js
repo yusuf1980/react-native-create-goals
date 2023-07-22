0b287d68a5b9502186d7bdf52b7c4abeb94e90fd
yusuf1980 <yusufachmad1980@gmail.com>
2023-07-22 14:55:05 +0800

    add image

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
+import { StyleSheet, View, TextInput, Button, Modal, Image } from "react-native";
 
 function GoalInput(props) {
   const [enterGoalText, setEnterGoalText] = useState("");
@@ -16,6 +16,7 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require('../assets/images/goal.png')} />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -39,12 +40,15 @@ const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 24,
+    alignItems: "center", 
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    backgroundColor: '#311b6b'
   },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20
+  },    
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
